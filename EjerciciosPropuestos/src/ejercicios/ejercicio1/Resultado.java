5fa7894066b13ae00b15fd9f94fda6fdfaa9e8bb
O-Marin <omarancb@gmail.com>
2022-08-29 07:38:16 +1000

    se agrega validador de notas, se agrega calculo promedio, se agrega feedback aprobado/reprobado

--- EjerciciosPropuestos/src/ejercicios/ejercicio1/Resultado.java
+++ EjerciciosPropuestos/src/ejercicios/ejercicio1/Resultado.java
@@ -2,14 +2,14 @@ package ejercicios.ejercicio1;
 
 import java.util.Scanner;
 
-public class promedioDeNotas {
+public class Resultado {
 	public void promedio() {
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Ingresar 3 notas entre 1 y 7");
 		
 		System.out.println("nota 1");
-		int nota1 = sc.nextInt();
+		float nota1 = sc.nextFloat();
 		while ((nota1 < 1) || (nota1 > 7)){
 			System.out.println("Nota " + nota1 +  " invalida, por favor intentar nuevamente, Insertar una nota entre 1 y 7");
 			nota1 = sc.nextInt();
@@ -17,17 +17,34 @@ public class promedioDeNotas {
 		
 		
 		System.out.println("nota 2");
-		int nota2 = sc.nextInt();
+		float nota2 = sc.nextFloat();
 		while ((nota2 < 1) || (nota2 > 7)){
 			System.out.println("Nota " + nota2 +  " invalida, por favor intentar nuevamente, Insertar una nota entre 1 y 7");
 			nota2 = sc.nextInt();
 		}
 		
 		System.out.println("nota 3");
-		int nota3 = sc.nextInt();
+		float nota3 = sc.nextFloat();
 		while ((nota3 < 1) || (nota3 > 7)){
 			System.out.println("Nota " + nota3 +  " invalida, por favor intentar nuevamente, Insertar una nota entre 1 y 7");
 			nota3 = sc.nextInt();
 		}
+		
+		
+		float promedio = (nota1 + nota2 + nota3)/3;	
+		
+		
+		if (promedio < 4) {
+			System.out.println("El promedio del alumno es: "+ promedio + " el alumno a reprobado");
+		}else if (promedio >= 4) {
+			System.out.println("El promedio del alumno es: "+ promedio + " el alumno a aprobado");
+		}
+		 
+			
+		sc.close();
+		
+		
+		
+		}
 	}
-}
+
--- EjerciciosPropuestos/src/ejercicios/main/Main.java
+++ EjerciciosPropuestos/src/ejercicios/main/Main.java
@@ -1,10 +1,15 @@
 package ejercicios.main;
 
+import ejercicios.ejercicio1.Resultado;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Resultado resultado = new Resultado();
 		
-
+		resultado.promedio();
+		
+		
 	}
 
 }
