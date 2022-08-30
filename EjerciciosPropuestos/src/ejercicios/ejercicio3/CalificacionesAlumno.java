689f651725d35d106211fd8dbe8cafbec268d476
O-Marin <omarancb@gmail.com>
2022-08-30 13:05:40 +1000

    se agrega input de info del alumno, se agrega validadores de nota

--- EjerciciosPropuestos/src/ejercicios/ejercicio3/CalificacionesAlumno.java
+++ EjerciciosPropuestos/src/ejercicios/ejercicio3/CalificacionesAlumno.java
@@ -1,8 +1,46 @@
 package ejercicios.ejercicio3;
 
+import java.util.Scanner;
+
 public class CalificacionesAlumno {
 	public void calificacion() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ingresar nombre alumno");
+		String nombre = sc.nextLine();
 		
+		System.out.println("Ingresar apellido alumno");
+		String apellido = sc.nextLine();
+		
+		System.out.println("Ingresar asignatura");
+		String asignatura = sc.nextLine();
+		
+		System.out.println("Ingresar apellido del docente a cargo");
+		String apellidoDocente = sc.nextLine();
+		
+		System.out.println("Ingresar primera nota");
+		float nota1 = sc.nextFloat();
+		while ((nota1 < 1) || (nota1 > 7)){
+			System.out.println("Nota " + nota1 +  " invalida, por favor intentar nuevamente, Insertar una nota entre 1 y 7");
+			nota1 = sc.nextFloat();
+		}
+		
+		System.out.println("Ingresar segunda nota");
+		float nota2 = sc.nextFloat();
+		while ((nota2 < 1) || (nota2 > 7)){
+			System.out.println("Nota " + nota2 +  " invalida, por favor intentar nuevamente, Insertar una nota entre 1 y 7");
+			nota2 = sc.nextFloat();
+		}
+		
+		System.out.println("Ingresar tercera nota");
+		float nota3 = sc.nextFloat();
+		while ((nota3 < 1) || (nota3 > 7)){
+			System.out.println("Nota " + nota3 +  " invalida, por favor intentar nuevamente, Insertar una nota entre 1 y 7");
+			nota3 = sc.nextFloat();
+		}
+		
+		String ficha = String.format("Alumno: %s %s\n Asignatura: %s\n Docente: Sr./Sra. %s\n Nota 1: %.1f\n Nota 2: %.1f\n Nota 3: %.1f", nombre, apellido, asignatura, apellidoDocente,nota1,nota2,nota3);
+		System.out.println(ficha);
+	
 		
 	}
 }
--- EjerciciosPropuestos/src/ejercicios/main/Main.java
+++ EjerciciosPropuestos/src/ejercicios/main/Main.java
@@ -2,12 +2,14 @@ package ejercicios.main;
 
 import ejercicios.ejercicio1.Resultado;
 import ejercicios.ejercicio2.Alumno;
+import ejercicios.ejercicio3.CalificacionesAlumno;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Resultado resultado = new Resultado();
 		Alumno alumno1 = new Alumno();
+		CalificacionesAlumno promedioAlumno = new CalificacionesAlumno();
 		
 		//resultado.promedio();
 		
