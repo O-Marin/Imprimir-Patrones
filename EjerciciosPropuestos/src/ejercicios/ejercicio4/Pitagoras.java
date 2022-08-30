7ad7dd37b3b3072b804bb132b3db4918c5c9b383
O-Marin <omarancb@gmail.com>
2022-08-31 08:53:06 +1000

    se crea proyecto calculo hipotenusa, se agregan input usuario y variables catetos

---
+++ EjerciciosPropuestos/src/ejercicios/ejercicio4/Pitagoras.java
@@ -0,0 +1,13 @@
+package ejercicios.ejercicio4;
+
+import java.util.Scanner;
+
+public class Pitagoras {
+	public void calcularHipotenusa(){
+		Scanner sc = new Scanner(System.in);
+		double catOp = sc.nextDouble();
+		double catAdy = sc.nextDouble();
+		
+		
+	}
+}
