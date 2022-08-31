fe7aee87b05536022462441e20dd837f877930e0
O-Marin <omarancb@gmail.com>
2022-09-01 09:02:46 +1000

    terminado patron 2 ejercicio 2

--- desafioEvaluadoPatrones/src/desafioEvaluadoPatrones/Patron2.java
+++ desafioEvaluadoPatrones/src/desafioEvaluadoPatrones/Patron2.java
@@ -28,15 +28,37 @@ public class Patron2 {
 		}
 		sc.close();
 
-		}
-
-	
+	}
 
 	// prueba patron 1
 	public static void patron2() {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ingresar valor n");
+		int n = sc.nextInt();
+		int x = 0;
+		while (x < n) {
+			if (x == 0 || x == n - 1) {
+				for (int i = 1; i <= n; i++) {
+					System.out.print("*");
+				}
+			} else if (x != 0 || x != n - 1) {
+				int j = n;
+				for (int i = 1; i <= n; i++) {
+
+					if (i == j - x) {
+						System.out.print("*");
+					} else if (i != j - x) {
+						System.out.print(" ");
+					}
+				}
+				j--;
+			}
+			System.out.println();
+			x++;
+		}
 	}
+
 	public static void patron3() {
-		
+
 	}
 }
