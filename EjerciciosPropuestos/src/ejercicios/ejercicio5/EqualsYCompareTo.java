5b23e044291cbad887aa646b290a5528a8ddcaf4
O-Marin <omarancb@gmail.com>
2022-08-31 11:25:28 +1000

    se crea ejercicio 5, se crean metodos y invocacion en main

---
+++ EjerciciosPropuestos/src/ejercicios/ejercicio5/EqualsYCompareTo.java
@@ -0,0 +1,12 @@
+package ejercicios.ejercicio5;
+
+public class EqualsYCompareTo {
+	public void igual() {
+		
+	}
+	
+	public void comparado() {
+		
+		
+	}
+}
--- EjerciciosPropuestos/src/ejercicios/main/Main.java
+++ EjerciciosPropuestos/src/ejercicios/main/Main.java
@@ -4,6 +4,7 @@ import ejercicios.ejercicio1.Resultado;
 import ejercicios.ejercicio2.Alumno;
 import ejercicios.ejercicio3.CalificacionesAlumno;
 import ejercicios.ejercicio4.Pitagoras;
+import ejercicios.ejercicio5.EqualsYCompareTo;
 
 public class Main {
 
@@ -12,12 +13,17 @@ public class Main {
 		Alumno alumno1 = new Alumno();
 		CalificacionesAlumno promedioAlumno = new CalificacionesAlumno();
 		Pitagoras hip = new Pitagoras();
+		EqualsYCompareTo comparacion = new EqualsYCompareTo();
+		
 		
 		//resultado.promedio();
-		
 		//alumno1.fichaAlumno();
-		//promedioAlumno.calificacion();
-		hip.calcularHipotenusa();
+		
+		
+		//promedioAlumno.calificacion();//este metodo incluye los ejercicios anteriores
+		//hip.calcularHipotenusa();
+		comparacion.igual();
+		comparacion.comparado();
 		
 		
 	}
