2c9e35fe5b0ad118f7e70669bafeddf2aae2862a
O-Marin <omarancb@gmail.com>
2022-09-02 02:59:37 +1000

    agregado patron 4 y ultimo  del ejercicio 2

--- desafioEvaluadoPatrones/src/desafioEvaluadoPatrones/Main.java
+++ desafioEvaluadoPatrones/src/desafioEvaluadoPatrones/Main.java
@@ -15,7 +15,36 @@ public class Main {
 		 * Patron2.patron1(); System.out.println(); Patron2.patron2();
 		 * System.out.println();
 		 */
-		Patron2.patron3();System.out.println();
-		
+		// Patron2.patron3();System.out.println();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Insertar un valor para n");
+		int n = sc.nextInt();
+		int x = n - 2;
+
+		for (int i = 1; i <= n; i++) {
+			if (i != 1 && i == n) {
+				System.out.print(" ");
+			} else {
+				System.out.print("*");
+			}
+		}
+		System.out.println();
+		for (int j = 1; j <= x; j++) {
+			for (int i = 1; i <= n; i++) {
+				if (i == 1 || i == n) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+
+		for (int i = 1; i <= n; i++) {
+			if (i == 1) {
+				System.out.print(" ");
+			} else
+				System.out.print("*");
+		}
 	}
 }
--- desafioEvaluadoPatrones/src/desafioEvaluadoPatrones/Patron2.java
+++ desafioEvaluadoPatrones/src/desafioEvaluadoPatrones/Patron2.java
@@ -82,4 +82,37 @@ public class Patron2 {
 		}
 
 	}
+	public static void patron4() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Insertar un valor para n");
+		int n = sc.nextInt();
+		int x = n - 2;
+
+		for (int i = 1; i <= n; i++) {
+			if (i != 1 && i == n) {
+				System.out.print(" ");
+			} else {
+				System.out.print("*");
+			}
+		}
+		System.out.println();
+		for (int j = 1; j <= x; j++) {
+			for (int i = 1; i <= n; i++) {
+				if (i == 1 || i == n) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+
+		for (int i = 1; i <= n; i++) {
+			if (i == 1) {
+				System.out.print(" ");
+			} else
+				System.out.print("*");
+		}
+		sc.close();
+	}
 }
